6efa37a6d0b561afd3f22d6dd8b2c56697834140
Riya0899 <139361089+Riya0899@users.noreply.github.com>
2025-01-15 10:43:02 +0000

    conversion

---
+++ basics_js/conversion.js
@@ -0,0 +1,22 @@
+let score="3abc"
+console.log(typeof score);//number
+
+let valueinNumber=Number(score)//convertion
+console.log(typeof valueinNumber);
+console.log(valueinNumber);//not a number
+
+
+// "33"=> 33
+// "33abc"=>NaN
+// true=>1 false=>0
+let isloggedin=1
+
+let boolean_value=Boolean(isloggedin)//converted into true
+console.log(boolean_value);
+//1=>true 0=>false ""=>false "riya"=>true
+
+let numberis=45
+let stringnumber=String(numberis)
+console.log(stringnumber);//converted into string
+console.log(typeof stringnumber);
+
