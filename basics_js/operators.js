474db1f42407ac9c93337b758cb1131e2fb7d5fa
Riya0899 <139361089+Riya0899@users.noreply.github.com>
2025-01-15 10:44:31 +0000

    operators

---
+++ basics_js/operators.js
@@ -0,0 +1,27 @@
+console.log(3+3);
+console.log(3-3);
+console.log(3*3);
+console.log(3**3);
+console.log(3/3);
+console.log(3%3);
+//Arithmetic operations
+
+str1="hello"
+str2=" riya"
+console.log(str1+str2);
+//concatinate two strings
+
+console.log("1"+2);//string first=>12
+console.log(1+"2");//number first=>12
+console.log("1"+2+2);//string first=>122
+console.log(2+2+"1");//number first=>41
+//whatever type of value is there first it will follow that conversion
+
+console.log(+true);//1=>conversion
+console.log(+"");//0=>conversion
+
+gamecounter=100
+gamecounter++
+console.log(gamecounter);
+
+
