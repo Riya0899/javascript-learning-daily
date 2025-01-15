ef5d58c7a2db0ac575cabf0aca9dbb459c5a7a34
Riya0899 <139361089+Riya0899@users.noreply.github.com>
2025-01-15 10:38:50 +0000

    keywords

---
+++ basics_js/keywords.js
@@ -0,0 +1,16 @@
+const accountid=12345
+let name="riya"
+var email="riya@gmail.com"
+password="123456"
+let age;
+
+// accountid=1234 // not allowed // const
+name="Priya"
+email="Priya@gmail.com"
+password="1234567"
+
+console.table([name,email,password,age]);
+// to execute multiple values
+/* 
+prefer not to use var because of issue in block scope and functional scope
+*/
