11c79c900fb550d64746abd044b1b1b53d58d56e
Riya0899 <139361089+Riya0899@users.noreply.github.com>
2025-01-15 10:41:42 +0000

    datatypes

---
+++ basics_js/datatypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat the code as the newer version of JS
+
+// alert(4+4) //we are using nodejs not browser
+
+let name="riya"
+// age=21
+const isloggedin=false
+let state;
+let temp=null
+
+//number->range 2 to the power 53
+// bigint->for larger values
+//string->""
+//boolean->true/false
+//NULL->stand alone value
+//undefined->
+//symbol->unique
+
+//objects
+
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
