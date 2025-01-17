301b7fde6124397205639b2c1a57570d7e3489c7
Your Name <you@example.com>
2025-01-17 21:54:55 +0530

    for-in-loop

---
+++ control-flow_js/for-in.js
@@ -0,0 +1,16 @@
+const myobj={
+    name:"riya",
+    age:21,
+    gender:"female"
+
+}
+for (const key in myobj) {
+ 
+        console.log(`key is ${key} and value is ${myobj[key]}`);
+        
+    }
+const arr=["riya","priya","supriya"]
+for (const key in arr) {
+    console.log(arr[key]);
+    
+    }
