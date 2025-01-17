63bbdfbf37f2a36b1c15e36a0daaa0b4d2a6de7c
Your Name <you@example.com>
2025-01-17 20:13:14 +0530

    while-dowhile-loops

---
+++ control-flow_js/while-dowhile.js
@@ -0,0 +1,24 @@
+//while loop
+let index=0    //initialization
+while (index<10) {    //condition
+    console.log(`value of index is ${index}`);
+    index=index+2  //increment decrement or others
+}
+
+//while loop on array
+myarray=[1,2,3,4,5]
+let arr=0
+while (arr<myarray.length) {
+    console.log(`value is ${myarray[arr]}`);
+    arr=arr+2
+}
+
+//dowhile loop
+let score=11
+do{
+console.log(`score is ${score }`);  
+score++
+}
+while (score<=10) {
+    
+}
